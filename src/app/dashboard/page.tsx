a0612f85e3ed77ba04894dd8cdf021c32bef717b
Weronika Jankowska <wjankowska8@gmail.com>
2025-03-03 23:01:44 +0000

    ✨ Added PieCharts

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import React from 'react'
+import { PieChart } from '@/components/charts/PieCharts'
+
+const DashboardPage = () => {
+  return (
+    <div>
+      <PieChart />
+    </div>
+  )
+}
+
+export default DashboardPage
